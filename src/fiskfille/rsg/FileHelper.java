330df66d957b50268767f58bb5d5fb92032cfee3
iLexiconn <ilexiconn@gmail.com>
2015-04-22 10:40:16 +0200

    *cough* spaces not tabs *cough*

--- src/fiskfille/rsg/FileHelper.java
+++ src/fiskfille/rsg/FileHelper.java
@@ -9,41 +9,40 @@ import java.util.Random;
 
 public class FileHelper
 {
-	public static ArrayList<String> readFile(String fileDirectory)
-	{
-		String s;
-		ArrayList<String> list = new ArrayList<String>();
-		
-		try
-		{
-			fileDirectory = "categories/" + fileDirectory;
-			File file = new File(fileDirectory);
-			
-			if (!file.exists())
-			{
-				file.createNewFile();
-			}
-			
-			@SuppressWarnings("resource")
-			BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(fileDirectory)));
-			
-			while ((s = input.readLine()) != null)
-			{
-				list.add(s);
-			}
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-		
-		return list;
-	}
-	
-	public static String getRandomEntry(String fileDirectory)
-	{
-		Random rand = new Random();
-		ArrayList<String> list = readFile(fileDirectory);
-		return list.get(rand.nextInt(list.size()));
-	}
+    public static ArrayList<String> readFile(String fileDirectory)
+    {
+        String s;
+        ArrayList<String> list = new ArrayList<>();
+
+        try
+        {
+            fileDirectory = "categories/" + fileDirectory;
+            File file = new File(fileDirectory);
+
+            if (!file.exists())
+            {
+                file.createNewFile();
+            }
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(fileDirectory)));
+
+            while ((s = input.readLine()) != null)
+            {
+                list.add(s);
+            }
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+
+    public static String getRandomEntry(String fileDirectory)
+    {
+        Random rand = new Random();
+        ArrayList<String> list = readFile(fileDirectory);
+        return list.get(rand.nextInt(list.size()));
+    }
 }
--- src/fiskfille/rsg/Main.java
+++ src/fiskfille/rsg/Main.java
@@ -4,70 +4,76 @@ import java.io.File;
 
 public class Main
 {
-	public static String[] keys;// = {"people", "sentences", "statements", "verbs", "things", "food", "adverbs", "places", "adjectives", "times", "emotions"};
-	
-	public static void main(String[] args)
-	{
-		File categories = new File("categories");
-		
-		File[] listFiles = categories.listFiles();
-		keys = new String[listFiles.length];
-		
-		for (int i = 0; i < listFiles.length; i++)
-		{
-			keys[i] = listFiles[i].getName().split("\\.")[0];
-		}
+    public static String[] keys;
 
-		if (args.length > 0) for (int i = 0; i < parseInt(args[0]); i++) System.out.println(generateRandomSentence());
-		else System.out.println(generateRandomSentence());
-	}
-	
-	private static String generateRandomSentence()
-	{
-		String sentence = FileHelper.getRandomEntry("sentences.txt");
-		
-		while (sentence.contains("[") && sentence.contains("]"))
-		{
-			sentence = format(sentence);
-		}
-		
-		return sentence + ".";
-	}
+    public static void main(String[] args)
+    {
+        File categories = new File("categories");
 
-	public static String format(String s)
-	{
-		String[] words = s.split(" ");
-		String text = "";
-		
-		for (int i = 0; i < words.length; ++i)
-		{
-			String word = words[i];
-			boolean name = word.equals("[people]");
-			
-			for (String key : keys)
-			{
-				if (word.equals("[" + key + "]"))
-				{
-					String randomEntry = FileHelper.getRandomEntry(word.replace("[", "").replace("]", "") + ".txt");
-					word = word.replace(word, randomEntry);
-				}
-			}
-			
-			text += name ? word + " " : (i == 0 ? word.substring(0, 1).toUpperCase() + word.substring(1) : word) + " ";
-		}
-		
-		return text.substring(0, text.length() - 1);
-	}
+        File[] listFiles = categories.listFiles();
+        keys = new String[listFiles.length];
 
-	public static int parseInt(String value)
-	{
-		try
-		{
-			return Integer.parseInt(value);
-		}
-		catch(NumberFormatException nfe)
-		{
-			return 1;
-		}
-	}
+        for (int i = 0; i < listFiles.length; i++)
+        {
+            keys[i] = listFiles[i].getName().split("\\.")[0];
+        }
+
+        if (args.length > 0)
+        {
+            for (int i = 0; i < parseInt(args[0]); i++)
+            {
+                System.out.println(generateRandomSentence());
+            }
+        }
+        else System.out.println(generateRandomSentence());
+    }
+
+    private static String generateRandomSentence()
+    {
+        String sentence = FileHelper.getRandomEntry("sentences.txt");
+
+        while (sentence.contains("[") && sentence.contains("]"))
+        {
+            sentence = format(sentence);
+        }
+
+        return sentence + ".";
+    }
+
+    public static String format(String s)
+    {
+        String[] words = s.split(" ");
+        String text = "";
+
+        for (int i = 0; i < words.length; ++i)
+        {
+            String word = words[i];
+            boolean name = word.equals("[people]");
+
+            for (String key : keys)
+            {
+                if (word.equals("[" + key + "]"))
+                {
+                    String randomEntry = FileHelper.getRandomEntry(word.replace("[", "").replace("]", "") + ".txt");
+                    word = word.replace(word, randomEntry);
+                }
+            }
+
+            text += name ? word + " " : (i == 0 ? word.substring(0, 1).toUpperCase() + word.substring(1) : word) + " ";
+        }
+
+        return text.substring(0, text.length() - 1);
+    }
+
+    public static int parseInt(String value)
+    {
+        try
+        {
+            return Integer.parseInt(value);
+        }
+        catch (NumberFormatException nfe)
+        {
+            return 1;
+        }
+    }
 }
